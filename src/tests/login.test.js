1513ecac4213929f8d512670d2ee3340da1a08bb
cleytonRR <cleytonrodrigues69@live.com>
2020-02-05 18:03:52 -0300

    Ensure verification token

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -4,17 +4,33 @@ const request = require('supertest')
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const GeneratorToken = require('../util/generatorToken')
+const VerifyToken = require('../util/verifyToken')
 
 const mockUser = {
   id: 1,
   email: 'any_email@gmail.com'
 }
 
+var invalidToken = ''
+var validtoken = ''
+
 describe.only('Suite tests for ensure correct login', function () {
   it('Ensure return a token basead in email and id of user', () => {
     const response = GeneratorToken.token(mockUser.id, mockUser.email)
+    validtoken = response
+    invalidToken = 's' + response
     var decoded = jwt.verify(response, process.env.JWT_KEY)
     assert.deepStrictEqual(mockUser.id, decoded.id)
     assert.deepStrictEqual(mockUser.email, decoded.email)
+  })
+
+  it('Return error if token is provided is invalid', () => {
+    const result = VerifyToken.verify(invalidToken)
+    assert.deepStrictEqual(false, result)
+  })
+
+  it('Return true if token provided is valid', () => {
+    const result = VerifyToken.verify(validtoken)
+    assert.deepStrictEqual(true, result)
   })
 })
